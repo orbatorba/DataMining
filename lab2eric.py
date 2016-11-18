5ca990a8532c7e59654e62e6b79249652df98a08
Eric Öhman <ericohman@n128-p210.kthopen.kth.se>
2016-11-18 17:02:02 +0100

    Functions and stuff lab2eric

--- lab2eric.py
+++ lab2eric.py
@@ -1,21 +1,25 @@
 
 
 lib = []
-corpus = [[1,2,3],[2,3],[2,3,4],[1,5],[1,2,5]]
-#for line in open('T10I4D100K.dat', 'r'):
-#    item = line.rstrip()
-#    temp = []
-#    for j in item.split(" "):
-#    	temp.append(int(j))
-#    	corpus.add(int(j))
-#    lib.append(temp)
+
+for line in open('T10I4D100K.dat', 'r'):
+    item = line.rstrip()
+    temp = []
+    for j in item.split(" "):
+    	temp.append(int(j))
+    lib.append(temp)
+#lib =  [[1,2,3],[2,3],[2,3,4],[1,5],[1,2,5]]
+def getCorpus(lib):
+	corpus = set([])
+	for bag in lib:
+		for value in bag:
+			corpus.add(value)
+	corpus = list(corpus)
+	corpus.sort()
+	return corpus
 
 
 
-
-def getKey(l):
-	l.sort()
-	return tuple(l)
 
 def getSingleTonTable(corpus,s):
 	table = {}
@@ -33,16 +37,86 @@ def getSingleTonTable(corpus,s):
 			del table[key]
 	return table
 
-def getTable(corpus,previous,n):
-	for bag in corpus:
-		
+bag = [1,2,3,4]
 
 
-print(getSingleTonTable(corpus,2))
+def getSubsets(bigSet,k,idx,subset,solution):
+	if len(subset)==k:
+		subset.sort()
+		solution.append(tuple(subset))
+		return
+
+	if idx==len(bigSet):
+		return
+
+	x = bigSet[idx]
+	subset.append(x)
+	getSubsets(bigSet, k, idx+1, subset, solution)
+	subset.remove(x)
+	getSubsets(bigSet, k, idx+1, subset, solution)
+
+
+def getTable(k,lib,s):
+	table = {}
+	for bag in lib:
+		perms = []
+		getSubsets(bag,k,0,[],perms)
+		for key in perms:
+			if key in table:
+				table[key]+=1
+			else:
+				table[key]=1
+
+	for key in table.keys():
+		if table[key]<s:
+			del table[key]
+	return table
+
+
+def convertToList(tuplelist):
+	for key in hej:
+		for val in key:
+			temp.append(val)
+
+
+def getTableAboveOne(k,lib,s,prev_table):
+	table = {}
+	valid_perms = []
+	bigSet = prev_table.keys()
+	temp = []
+	for key in bigSet:
+		for val in key:
+			temp.append(val)
+	getSubsets(temp,k,0,[],valid_perms)
+	valid_perms = set(valid_perms)
+
+	for bag in lib:
+		current_perms = []
+		getSubsets(bag,k,0,[],current_perms)
+		for perm in current_perms:
+			if perm in valid_perms:
+				if perm in table:
+					table[perm]+=1
+				else:
+					table[perm]=1
+
+	for key in table.keys():
+		if table[key]<s:
+			del table[key]
+	return table
 
 
 
+s = 500
+prev_table = getTable(1,lib,s)
+
+print(prev_table)
+print("\n\n")
 
 
-
+prev_table = getTableAboveOne(2,lib,s,prev_table)
+print(prev_table)
+print("\n\n")
+prev_table = getTableAboveOne(3,lib,s,prev_table)
+print(prev_table)
 
